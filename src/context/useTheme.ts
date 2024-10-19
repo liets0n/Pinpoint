a626e34328595fe3c4c1e0b495d3b6f4d12a4cd0
Lietson Dos Santos <dossantoslietson@gmail.com>
2024-10-19 01:28:33 +0100

    :sparkles: Persist the current theme

--- src/context/useTheme.ts
+++ src/context/useTheme.ts
@@ -1,11 +1,17 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 type State = {
   currentTheme: string
   changeCurrentTheme: (theme: string) => void
 }
 
-export const useTheme = create<State>()(set => ({
-  currentTheme: 'light',
-  changeCurrentTheme: theme => set({ currentTheme: theme })
-}))
+export const useTheme = create<State>()(
+  persist(
+    set => ({
+      currentTheme: 'light',
+      changeCurrentTheme: theme => set({ currentTheme: theme })
+    }),
+    { name: '@pinpoint/theme', version: 1 }
+  )
+)
